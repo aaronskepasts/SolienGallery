21626d9b84f250483c86c3d75db416fb0fb1d784
Gabriel Grajeda <gabriel.grajeda@gmail.com>
2021-11-01 20:46:16 -0400

    Initial Flask implementation with interface definitions
    Sample flask implementation, in case Python is easier to use in the backend.

---
+++ protos/flask-proto/server.py
@@ -0,0 +1,41 @@
+from sys import stderr
+from flask import Flask, request, make_response, render_template
+
+from solana_search import search
+from solana_util import SearchRequest, SearchResponse, SolanaNFT
+
+#-----------------------------------------------------------------------
+
+app = Flask(__name__, template_folder=".")
+frontend_path = "views/pages/"
+
+#-----------------------------------------------------------------------
+
+# Render and return the homepage.
+@app.route("/", methods=["GET"])
+@app.route("/index", methods=["GET"])
+def index():
+    html = render_template(frontend_path + "index.html")
+    response = make_response(html)
+    return response
+
+#-----------------------------------------------------------------------
+
+# Render and return the download page.
+@app.route("/download", methods=["GET"])
+def download():
+    html = render_template(frontend_path + "download.html")
+    response = make_response(html)
+    return response
+
+#-----------------------------------------------------------------------
+
+# Render and return the gallery builder page.
+@app.route("/alpha_gallery/<wallet>", methods=["GET"])
+def alpha_gallery(wallet):
+    search_req = SearchRequest(wallet)
+    search_res = search(search_req)
+    html = render_template(frontend_path + "alpha_gallery.html",
+                           wallet=wallet, search_res=search_res)
+    response = make_response(html)
+    return response
