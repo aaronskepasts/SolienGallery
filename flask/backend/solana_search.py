fee42e041ccc1ff0e891675b21d78f71a3b481bc
glebeau <glebeau@princeton.edu>
2021-11-10 17:36:56 -0500

    Searching by wallet rather than mint address enabled
    Changes were made to solana_search and solana_util to enable this change using the list of mint addresses of Soliens. Feel free to try using "Ca9ugqnNQisHaJzzGSYT6orWtbdYwK4qnKs5D2PhmXYC" as input to the search, for example.

--- flask/backend/solana_search.py
+++ flask/backend/solana_search.py
@@ -1,4 +1,4 @@
-from .solana_util import SearchResponse, SolanaNFT
+from .solana_util import SearchResponse, SolanaNFT, only_soliens
 from theblockchainapi import TheBlockchainAPIResource, SolanaNetwork
 
 #-----------------------------------------------------------------------
@@ -7,36 +7,42 @@ from theblockchainapi import TheBlockchainAPIResource, SolanaNetwork
 MY_API_KEY_ID = "i80Y9AzQjwfOQfR"
 MY_API_SECRET_KEY = "RIT7RKOVA2cQy5S"
 BLOCKCHAIN_API_RESOURCE = TheBlockchainAPIResource(
-    api_key_id=MY_API_KEY_ID,
-    api_secret_key=MY_API_SECRET_KEY
+	api_key_id=MY_API_KEY_ID,
+	api_secret_key=MY_API_SECRET_KEY
 )
 
 #-----------------------------------------------------------------------
 
-# Returns a list Solana NFT objects.
 def search(request):
 	try:
 		assert MY_API_KEY_ID is not None
 		assert MY_API_SECRET_KEY is not None
 	except AssertionError:
-		raise Exception("API keypair broken")
+		raise Exception("API keypair is broken")
 
-	# Aaron's Solien NFT Address = 'FN8EXxCE8Nty5h6iNtfdN8tqmCwFYiSuM6j8bLa9Uc5h'
-	# Ape NFT Address = 'GUqyT6hsve7TeJVR8NWZriPTVj6wZVkp9FZAsiHy3aYA'
 
-	# nft_addresses will be a list of Strings of NFT addresses
-	# to be retrieved from wallet
-	nft_addresses = [request.wallet]
+	# get all NFTs from given wallet
+	# e.g. public_key = "Kycg1YrNJ9ezMBErReAJJmHWVtVCaYEdvJbMBC1xhvm"
+	public_key = request.wallet
+	nfts = BLOCKCHAIN_API_RESOURCE.get_nfts_belonging_to_address(
+		public_key,
+		network=SolanaNetwork.MAINNET_BETA
+	)
+		
+
+	# filter out non-Solien NFTs
+	# fill in this list
+	nfts = only_soliens(nfts)
+
+	# create nft objects by searching for metadata at each address
+	# populate nft_list to be converted into a response and returned
 	nft_list = []
-	
-	for address in nft_addresses:
+	for address in nfts:
 		nft_metadata = BLOCKCHAIN_API_RESOURCE.get_nft_metadata(
 			mint_address=address,
 			network=SolanaNetwork.MAINNET_BETA
 		)
 		nft_list.append(SolanaNFT(address, nft_metadata))
 
-	for i in range(10):
-		nft_list.append(nft_list[0])
-
 	return SearchResponse(nft_list)
+
