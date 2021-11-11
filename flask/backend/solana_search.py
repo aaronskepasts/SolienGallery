d75142b310dc9d64480e10c23cb37f1eb0b1bce8
Aaron <ams10@princeton.edu>
2021-11-11 11:02:40 -0500

    Moving Soliens into JSON and changing search a bit

--- flask/backend/solana_search.py
+++ flask/backend/solana_search.py
@@ -1,11 +1,11 @@
-from .solana_util import SearchResponse, SolanaNFT, only_soliens
+from .solana_util import SearchResponse, SolanaNFT, query_soliens
 from theblockchainapi import TheBlockchainAPIResource, SolanaNetwork
 
 #-----------------------------------------------------------------------
 
 # Blockchain API key pair
-MY_API_KEY_ID = "i80Y9AzQjwfOQfR"
-MY_API_SECRET_KEY = "RIT7RKOVA2cQy5S"
+MY_API_KEY_ID = "j5I8K5JRLdm1G5e"
+MY_API_SECRET_KEY = "4TWbSQge5IthbcW"
 BLOCKCHAIN_API_RESOURCE = TheBlockchainAPIResource(
 	api_key_id=MY_API_KEY_ID,
 	api_secret_key=MY_API_SECRET_KEY
@@ -23,26 +23,30 @@ def search(request):
 
 	# get all NFTs from given wallet
 	# e.g. public_key = "Kycg1YrNJ9ezMBErReAJJmHWVtVCaYEdvJbMBC1xhvm"
+	# good public_key = "6KQDNrJoPJRa1UHX7C4Wf5FHgjvnswLMTePyUTySFKeQ"
 	public_key = request.wallet
-	nfts = BLOCKCHAIN_API_RESOURCE.get_nfts_belonging_to_address(
+	nft_addresses = BLOCKCHAIN_API_RESOURCE.get_nfts_belonging_to_address(
 		public_key,
 		network=SolanaNetwork.MAINNET_BETA
 	)
-		
+
 
 	# filter out non-Solien NFTs
 	# fill in this list
-	nfts = only_soliens(nfts)
+	solien_addresses = query_soliens(nft_addresses)
 
 	# create nft objects by searching for metadata at each address
 	# populate nft_list to be converted into a response and returned
 	nft_list = []
-	for address in nfts:
+	counter = 0
+	for address in solien_addresses:
 		nft_metadata = BLOCKCHAIN_API_RESOURCE.get_nft_metadata(
 			mint_address=address,
 			network=SolanaNetwork.MAINNET_BETA
 		)
 		nft_list.append(SolanaNFT(address, nft_metadata))
+		# counter += 1
+		# if counter == 2:
+		# 	break
 
 	return SearchResponse(nft_list)
-
