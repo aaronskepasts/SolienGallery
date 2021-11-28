c5dcdf7fca96db7d2ec1ac660be167fc2144cfdc
Gabriel Grajeda <gabriel.grajeda@gmail.com>
2021-11-28 18:41:37 -0500

    Change Redis server port

--- worker.py
+++ worker.py
@@ -4,14 +4,14 @@ from rq import Worker, Queue, Connection
 
 #-----------------------------------------------------------------------
 
-listen = ['high', 'default', 'low']
+listen = ["high", "default", "low"]
 
-redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
+redis_url = os.getenv("REDISTOGO_URL", "redis://localhost:10223")
 conn = redis.from_url(redis_url)
 
 #-----------------------------------------------------------------------
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     with Connection(conn):
         worker = Worker(map(Queue, listen))
         worker.work()
