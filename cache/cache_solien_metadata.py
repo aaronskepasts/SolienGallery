32079becc4c6712ae05eca2687d338941d65f261
Aaron <ams10@princeton.edu>
2021-11-28 12:12:03 -0500

    Adding cache files and cached soliens

---
+++ cache/cache_solien_metadata.py
@@ -0,0 +1,78 @@
+from theblockchainapi import TheBlockchainAPIResource, SolanaNetwork
+import urllib.request, json
+import threading
+from os.path import exists
+from json_validity import is_json_valid, write_invalid_output
+from threading import Thread
+
+MY_API_KEY_ID = "hQfwhpQgJfjxi6o"
+MY_API_SECRET_KEY = "yQW675BRAI5f4ci"
+BLOCKCHAIN_API_RESOURCE = TheBlockchainAPIResource(
+    api_key_id=MY_API_KEY_ID,
+    api_secret_key=MY_API_SECRET_KEY
+)
+
+def get_data_from_url(data_url):
+    with urllib.request.urlopen(data_url) as url:
+        data = json.loads(url.read().decode())
+        return(data)
+
+def cache_metadata(address):
+
+    # check if address.json exists in cache
+    validity = is_json_valid(address)
+    if validity[0] == 0:
+        return
+    elif validity[0] != -1:
+        print(validity[1])
+        return
+
+    # retrieve initial metadata from Solana
+    try:
+        nft_metadata = BLOCKCHAIN_API_RESOURCE.get_nft_metadata(
+            mint_address=address,
+            network=SolanaNetwork.MAINNET_BETA
+        )
+    except:
+        # store addresses that are broken somewhere
+        error = "Error getting nft_metadata using API for: " + str(address)
+        print(error)
+        write_invalid_output(error)
+        return
+
+    # retrive metadata from initial metadata uri
+    try:
+        metadata = get_data_from_url(nft_metadata['data']['uri'])
+    except:
+        error = "Error reading data from nft_metadata['data']['uri'] for: " + str(address)
+        print(error)
+        write_invalid_output(error)
+        return
+
+    # make new json file with address metadata
+    file_name = "soliens/" + address + ".json"
+    new_json = open(file_name, "w")
+    json.dump(metadata, new_json, indent=2)
+    new_json.close()
+    validity = is_json_valid(address)
+    if validity[0] != 0:
+        print(validity[1])
+
+def main():
+    try:
+        assert MY_API_KEY_ID is not None
+        assert MY_API_SECRET_KEY is not None
+    except AssertionError:
+        raise Exception("Fill in your key ID pair!")
+
+    with open('solien_addresses.json') as soliens_json:
+        addresses = json.load(soliens_json)
+    counter = 0
+    for address in addresses:
+        print(counter)
+        counter += 1
+        t = Thread(target=cache_metadata, args = (address,))
+        t.start()
+
+if __name__ == '__main__':
+    main()
---
+++ cache/json_validity.py
@@ -0,0 +1,69 @@
+from os.path import exists
+import json
+import time
+
+"""
+Return codes:
+(-1, json doesn't exist)
+(0, valid json exists)
+(1, invalid data)
+(2, incorrect number of key)
+(3, invalid file)
+"""
+
+def write_invalid_output(error):
+    out_file_name = "invalid" + str(time.strftime("%Y-%m-%d")) + ".txt"
+    with open(out_file_name, 'a') as out_file:
+        out_file.write(error + '\n')
+        out_file.close()
+
+def is_json_valid(address):
+
+    # check if json exists
+    if not exists("soliens/" + address + ".json"):
+        error = "file doesn't exist at: " + str(address)
+        write_invalid_output(error, address)
+        return (-1, error)
+
+    # check file is valid
+    try:
+        with open("soliens/" + address + '.json') as json_file:
+            json_dict = json.load(json_file)
+
+        # check data is not none in json
+        for key in json_dict:
+            if (json_dict[key]) is None:
+                error = "invalid data at: " + str(address)
+                write_invalid_output(error)
+                return (1, error)
+
+        # check correct num of keys
+        if len(json_dict) != 7:
+            error = "incorrect number of keys at: " + str(address)
+            write_invalid_output(error)
+            return (2, error)
+
+    # file is invalid
+    except:
+        error = "invalid file at: " + str(address)
+        write_invalid_output(error)
+        return (3, error)
+
+    # file valid
+    return (0, "valid json exists at: " + str(address))
+
+def main():
+    with open('solien_addresses.json') as soliens_json:
+        addresses = json.load(soliens_json)
+    counter = 0
+    for address in addresses:
+        validity = is_json_valid(address)
+        if validity[0] != 0:
+            print(validity[1])
+        counter += 1
+        if counter == len(addresses):
+            print(counter)
+
+
+if __name__ == '__main__':
+    main()
